93349cd9bbdcbaf176becd77056aa92dbbfa2387
irfanizudin <irfanizzudyn@gmail.com>
2022-12-04 18:51:26 +0700

    Initial Commit

---
+++ disclothes/disclothesApp.swift
@@ -0,0 +1,17 @@
+//
+//  disclothesApp.swift
+//  disclothes
+//
+//  Created by Irfan Izudin on 04/12/22.
+//
+
+import SwiftUI
+
+@main
+struct disclothesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
